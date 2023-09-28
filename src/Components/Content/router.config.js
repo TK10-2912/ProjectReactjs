0ed371cc4bc994573c2cd512c745799b8c0f06b0
DESKTOP-47KQM0G\Ngoc Anh <ngocanhqr12c@gmail.com>
2023-09-28 12:04:27 +0700

    thay doi vi tri home

--- src/Components/Content/router.config.js
+++ src/Components/Content/router.config.js
@@ -1,21 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import Home from '../../Home';
 import React from 'react';
-
+import Home from "../Home/Home";
 function Content() {
-    return <div>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/Store1" element={<div>Store1</div>}></Route>
-        <Route path="/Store2" element={<div>Store2</div>}></Route>
-        <Route path="/Store3" element={<div>Store3</div>}></Route>
-        <Route path="/Setting1" element={<div>Setting1</div>}></Route>
-        <Route path="/Setting2" element={<div>Setting2</div>}></Route>
-        <Route path="/Setting3" element={<div>Setting3</div>}></Route>
-        <Route path="/Setting4" element={<div>Setting4</div>}></Route>
-        
-      </Routes>
-    </div>
-  }
-
-export default Content ;
+  return <div>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/Store1" element={<div>Store1</div>}></Route>
+      <Route path="/Store2" element={<div>Store2</div>}></Route>
+      <Route path="/Store3" element={<div>Store3</div>}></Route>
+      <Route path="/Setting1" element={<div>Setting1</div>}></Route>
+      <Route path="/Setting2" element={<div>Setting2</div>}></Route>
+      <Route path="/Setting3" element={<div>Setting3</div>}></Route>
+      <Route path="/Setting4" element={<div>Setting4</div>}></Route>
+    </Routes>
+  </div>
+}
+export default Content;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 function Home (){
    
     return (  
-        <div>abcậck</div>
+        <div>Page Home</div>
     );
 }
  
