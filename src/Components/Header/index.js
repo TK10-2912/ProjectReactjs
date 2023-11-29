a9f2c83ab81939980804292f29e527e7cb050718
TIEN\ngoct <ngoctien06012002@gmail.com>
2023-11-29 09:27:14 +0700

    aaaaaaaaaaaaaaaaaaaaaaa

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,6 +4,7 @@ function Header() {
     return ( 
         <Row style={{ justifyContent:'center', alignItems:'center',backgroundColor:'#001529',height:'2%',width:'100%' }}>
             <span style={{color:'#fff'}}>This is my project</span>
+            <h1>22222222222222222222222222222222222222222</h1>
         </Row>
      );
 }
